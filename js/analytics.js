874413d3c5c689920fa87aba07f4688ca559ecf1
CYY <75060765+CYYHH3@users.noreply.github.com>
2022-09-27 11:17:38 +0800

    Add Analytics

---
+++ js/analytics.js
@@ -0,0 +1,31 @@
+// Analytics
+
+// Global site tag (gtag.js) - Google Analytics
+var ga = ga || [];
+(function() {
+    var gajs = document.createElement("script");
+    gajs.src = "https://www.googletagmanager.com/gtag/js?id=G-9CDP580SEV";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(gajs, s);
+})();
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-9CDP580SEV');
+// End Google Analytics
+
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?4a4ab0e8a5155c8f603264d571c2547a";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
+// End 百度统计
+
+// 「我要啦」网站流量统计
+!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JpOiKgqBZ8EBC372",ck:"JpOiKgqBZ8EBC372"});
+// End 「我要啦」网站流量统计
