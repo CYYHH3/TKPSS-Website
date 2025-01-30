71587f7dc26a391a7fe16540f1fecf31b4e8b539
CYY <75060765+CYYHH3@users.noreply.github.com>
2025-01-30 17:09:36 +0000

    重命名爬虫函数为 spider，并修复响应编码问题

--- .github/workflows/tkpss-web-spider.py
+++ .github/workflows/tkpss-web-spider.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-def crawl():
+def spider():
     url = 'http://tkpss.edu.hk'
     response = requests.get(url)
+    response.encoding = response.apparent_encoding
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # 在这里添加你的爬虫逻辑
     print(soup.title.string)
 
 if __name__ == '__main__':
-    crawl()
+    spider()
