da7ace8cce4e464ea38cdd495e95a41d79926704
CYY <75060765+CYYHH3@users.noreply.github.com>
2025-01-31 00:55:38 +0800

    Edit Action

--- .github/workflows/tkpss-web-spider.py
+++ .github/workflows/tkpss-web-spider.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import requests
+from bs4 import BeautifulSoup
+
+def crawl():
+    url = 'tkpss.edu.hk'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # 在这里添加你的爬虫逻辑
+    print(soup.title.string)
+
+if __name__ == '__main__':
+    crawl()
